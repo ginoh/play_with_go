fceeaad4f12d8af43a5000871b6a5cd6e11060e0
ginoh <s.ginoh@gmail.com>
2025-12-30 15:14:44 +0900

    imporove channel

--- samples/channel/sample1/main.go
+++ samples/channel/sample1/main.go
@@ -2,26 +2,30 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
-
 	// channel 作成
 	ch := make(chan int)
 
-	// channel は goroutine 間のデータ共有用
-	go receiver(ch)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go receiver(ch, &wg)
 
-	i := 0
-	for i < 100 {
+	for i := 0; i < 100; i++ {
 		ch <- i
-		i++
 	}
+	// 送信完了を通知して受信側のループを終了させる
+	close(ch)
+
+	// 受信側の処理完了を待つ
+	wg.Wait()
 }
 
-func receiver(ch <-chan int) {
-	for {
-		i := <-ch
+func receiver(ch <-chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := range ch {
 		fmt.Println(i)
 	}
 }
--- samples/channel/sample2/main.go
+++ samples/channel/sample2/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
 func main() {
@@ -12,28 +12,26 @@ func main() {
 
 	// channel は goroutine 間のデータ共有用
 	// 処理する receiver を 3つ用意
-	go receiver("rv1", ch)
-	go receiver("rv2", ch)
-	go receiver("rv3", ch)
+	var wg sync.WaitGroup
+	wg.Add(3)
+	go receiver("rv1", ch, &wg)
+	go receiver("rv2", ch, &wg)
+	go receiver("rv3", ch, &wg)
 
-	i := 0
-	for i < 100 {
+	for i := 0; i < 100; i++ {
 		ch <- i
-		i++
 	}
+	// 送信完了を通知して受信側のループを終了させる
 	close(ch)
 
-	// 処理が終わる余裕のために 3sec 待機
-	time.Sleep(3 * time.Second)
+	// 受信側の処理完了を待つ
+	wg.Wait()
 }
 
-func receiver(name string, ch <-chan int) {
-	for {
-		// バッファ空 && close の場合は ok == false
-		i, ok := <-ch
-		if ok == false {
-			break
-		}
+func receiver(name string, ch <-chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	// range ならチャネルがクローズされるまで受信し続ける
+	for i := range ch {
 		fmt.Println(name, i)
 	}
 	// rangeも利用できる、チャネルがクローズするとループから抜ける
