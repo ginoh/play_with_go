5ad343e8e8113c4e644d69e911d305d4d71379c6
ginoh <s.ginoh@gmail.com>
2025-12-30 22:16:59 +0900

    imporve embed

--- samples/embed/main.go
+++ samples/embed/main.go
@@ -4,21 +4,28 @@ import (
 	"embed"
 	"fmt"
 	"log"
+	"path"
 )
 
 //go:embed sources
 var assets embed.FS
 
 func main() {
-	listDir("sources", "./dst")
+	listDirRecursive("sources")
 }
 
-func listDir(srcDir, dstDir string) {
-	entries, err := assets.ReadDir("sources")
+func listDirRecursive(dir string) {
+	entries, err := assets.ReadDir(dir)
 	if err != nil {
 		log.Fatal(err)
 	}
 	for _, entry := range entries {
-		fmt.Printf("src=%s\n", entry.Name())
+		// embed FS 内の相対パスを組み立てる
+		fullPath := path.Join(dir, entry.Name())
+		if entry.IsDir() {
+			listDirRecursive(fullPath)
+			continue
+		}
+		fmt.Printf("src=%s\n", fullPath)
 	}
 }
